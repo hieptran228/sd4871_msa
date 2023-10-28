2713369cfc53a217ef5ce2d7226945e29dcf7402
Tran Hoang Hiep <hiepgia@gmail.com>
2023-10-29 01:29:01 +0700

    3.0.5

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -48,7 +48,7 @@ export default class App extends React.Component {
         <div className="container-fluid">
           <div className="row">
             <div className="col-xs-12 col-sm-8 col-md-8 offset-md-2">
-              <h1>Todos - V3.0.2 - PiSharp</h1>
+              <h1>Todos - V3.0.5 - PiSharp</h1>
               <div className="todo-app">
                 <AddTodo handleAddTodo={this.handleAddTodo} />
                 <TodoList todos={this.state.todos} />
